007842779ea74063d3dfbaf94e6cff3665f5594e
Praful Dhabekar <pddhabekar@gmail.com>
2017-12-21 10:56:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/praf/apidemo/app/AppConfig.java
@@ -0,0 +1,15 @@
+package com.example.praf.apidemo.app;
+
+/**
+ * Created by praf on 19-12-2017.
+ */
+
+public class AppConfig {
+
+    //Server user login url
+    public static final String URL_LOGIN = "http://192.168.31.32/android_login_api/login.php";
+
+    //Server register url
+    public static final String URL_REGISTER = "http://192.168.31.32/android_login_api/register.php";
+
+}
